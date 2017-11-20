107cbfcd72c2a501014b677f723e42b4d4b3b4c4
eli block <eliblock@yahoo.com>
2017-11-20 15:56:32 -0800

    ui

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://embeds.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://www.browzable.com' }
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
