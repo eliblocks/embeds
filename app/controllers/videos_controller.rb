3a13ea892ca6d2096ce7c44f4551c6f93b40ac2f
eli block <eliblock@yahoo.com>
2018-01-17 12:38:22 -0800

    cookies test

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -110,23 +110,23 @@ class VideosController < ApplicationController
       )
 
       url = "https://*.browzable.com/*"
-      Rails.env == 'development' ? domain = 'localhost' : domain = 'browzable.com'
+      domain = 'browzable.com'
 
       cloudfront_cookies = signer.signed_cookie(url, policy: cookie_policy.to_json)
 
       cookies['CloudFront-Policy'] = {
         value: cloudfront_cookies['CloudFront-Policy'],
-        domain: domain,
+        domain: :all,
         expires: 1.days.from_now
       }
       cookies['CloudFront-Key-Pair-Id'] = {
         value: cloudfront_cookies['CloudFront-Key-Pair-Id'],
-        domain: domain,
+        domain: :all,
         expires: 1.days.from_now
       }
       cookies['CloudFront-Signature'] = {
         value: cloudfront_cookies['CloudFront-Signature'],
-        domain: domain,
+        domain: :all,
         expires: 1.days.from_now
       }
     end
